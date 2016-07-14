bd7a1730d2406577fdbf053f33c6b56f7735bc30
Edwin Khoo <edwinksl@gmail.com>
2016-07-14 16:17:07 -0400

    Add code for Problem 2

---
+++ problem_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+
+def main(number):
+    a_1 = 1
+    a_2 = 2
+    a = a_1 + a_2
+    result = 2
+    while a < number:
+        a_1 = a_2
+        a_2 = a
+        a = a_1 + a_2
+        if a % 2 == 0:
+            result += a
+    return result
+
+if __name__ == '__main__':
+    print(main(int(sys.argv[1])))
