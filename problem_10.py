18d325babbc136c964cf6de7b758d1425d770276
Edwin Khoo <edwinksl@gmail.com>
2016-07-14 00:38:04 -0400

    Improve speed of brute-force search

--- problem_10.py
+++ problem_10.py
@@ -5,16 +5,19 @@ import sys
 
 def main(number):
     prime_numbers = [2]
-    for i in range(2, number):
+    for i in range(3, number, 2):
         if is_prime(i, prime_numbers):
             prime_numbers.append(i)
     return sum(prime_numbers)
 
 
 def is_prime(number, prime_numbers):
+    cutoff = int(number**(1/2))
     for i in prime_numbers:
         if number % i == 0:
             return False
+        if i >= cutoff:
+            return True
     return True
 
 if __name__ == '__main__':
