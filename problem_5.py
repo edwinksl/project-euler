14a7b14dc5fff8b53715354a992ecd1446719fab
Edwin Khoo <edwinksl@gmail.com>
2016-07-14 16:47:32 -0400

    Add code for Problem 5

---
+++ problem_5.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def main(upper_bound):
+    result = upper_bound
+    while True:
+        if not is_evenly_divisible(result, upper_bound):
+            result += 1
+        else:
+            break
+    return result
+
+
+def is_evenly_divisible(number, upper_bound):
+    for i in range(1, upper_bound):
+        if number % i != 0:
+            return False
+    return True
+
+if __name__ == '__main__':
+    print(main(int(sys.argv[1])))
