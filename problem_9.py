fccb61a410a692b5a411a2621f59826250fe9cb0
Edwin Khoo <edwinksl@gmail.com>
2016-07-14 23:06:30 -0400

    Add code for Problem 9

---
+++ problem_9.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def main(constraint):
+    for i in range(1, constraint+1):
+        for j in range(i, constraint+1):
+            k = constraint - i - j
+            if i**2 + j**2 == k**2:
+                return i*j*k
+
+if __name__ == '__main__':
+    print(main(int(sys.argv[1])))
