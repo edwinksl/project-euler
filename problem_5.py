06fff445c17539af388069167c7336439d47b496
Edwin Khoo <edwinksl@gmail.com>
2016-07-14 22:41:45 -0400

    Refactor code to make it faster

--- problem_5.py
+++ problem_5.py
@@ -2,21 +2,23 @@
 
 import sys
 
+from problem_3 import get_prime_factors
+
 
 def main(upper_bound):
-    result = upper_bound
-    while True:
-        if not is_evenly_divisible(result, upper_bound):
-            result += 1
-        else:
-            return result
-
-
-def is_evenly_divisible(number, upper_bound):
-    for i in range(1, upper_bound):
-        if number % i != 0:
-            return False
-    return True
+    factors = {}
+    for i in range(2, upper_bound+1):
+        prime_factors = get_prime_factors(i)
+        for j in prime_factors:
+            if j not in factors:
+                factors[j] = prime_factors[j]
+            else:
+                if prime_factors[j] > factors[j]:
+                    factors[j] = prime_factors[j]
+    result = 1
+    for prime_factor, exponent in factors.items():
+        result *= prime_factor**exponent
+    return result
 
 if __name__ == '__main__':
     print(main(int(sys.argv[1])))
