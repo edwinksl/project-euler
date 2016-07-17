8d5322037833b4baf326e57910de2e6fd18de761
Edwin Khoo <edwinksl@gmail.com>
2016-07-17 18:00:05 -0400

    Break up main function in 2 functions

--- problem_10.py
+++ problem_10.py
@@ -4,14 +4,6 @@ import sys
 import time
 
 
-def main(number):
-    prime_numbers = [2]
-    for i in range(3, number, 2):
-        if is_prime(i, prime_numbers):
-            prime_numbers.append(i)
-    return sum(prime_numbers)
-
-
 def is_prime(number, prime_numbers):
     cutoff = int(number**(1/2))
     for i in prime_numbers:
@@ -19,6 +11,18 @@ def is_prime(number, prime_numbers):
             return False
         if i >= cutoff:
             return True
+
+
+def get_prime_numbers(number):
+    prime_numbers = [2]
+    for i in range(3, number, 2):
+        if is_prime(i, prime_numbers):
+            prime_numbers.append(i)
+    return prime_numbers
+
+
+def main(number):
+    return sum(get_prime_numbers(number))
 
 if __name__ == '__main__':
     t_0 = time.time()
