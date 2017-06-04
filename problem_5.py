4498f1f114bfc146cad173bbabf78671efbb6097
Edwin Khoo <edwinksl@gmail.com>
2017-06-04 18:24:38 -0400

    Add newline and spaces

--- problem_5.py
+++ problem_5.py
@@ -7,7 +7,7 @@ from problem_3 import get_prime_factors
 
 def main(upper_bound):
     factors = {}
-    for i in range(2, upper_bound+1):
+    for i in range(2, upper_bound + 1):
         prime_factors = get_prime_factors(i)
         for j in prime_factors:
             if j not in factors:
@@ -19,6 +19,7 @@ def main(upper_bound):
     for prime_factor, exponent in factors.items():
         result *= prime_factor**exponent
     return result
+
 
 if __name__ == '__main__':
     print(main(int(sys.argv[1])))
