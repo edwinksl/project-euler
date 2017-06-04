c7da32255e7e87b08527c16b2f1673ec1c61e1e1
Edwin Khoo <edwinksl@gmail.com>
2017-06-04 18:25:23 -0400

    Add newline and spaces

--- problem_6.py
+++ problem_6.py
@@ -6,12 +6,13 @@ import sys
 def main(number):
     sum_of_squares = 0
     square_of_sum = 0
-    for i in range(number+1):
+    for i in range(number + 1):
         sum_of_squares += i**2
         square_of_sum += i
     square_of_sum *= square_of_sum
     diff = square_of_sum - sum_of_squares
     return diff
+
 
 if __name__ == '__main__':
     print(main(int(sys.argv[1])))
