0a0d4fccee52ea65303b1b33974bf701333de5e8
Edwin Khoo <edwinksl@gmail.com>
2017-06-04 18:24:04 -0400

    Add newline and spaces

--- problem_4.py
+++ problem_4.py
@@ -5,11 +5,12 @@ def main():
     result = 1
     for i in range(1, 1000):
         for j in range(i, 1000):
-            temp_result = i*j
+            temp_result = i * j
             temp_str = str(temp_result)
             if temp_str == temp_str[::-1] and temp_result > result:
                 result = temp_result
     return result
+
 
 if __name__ == '__main__':
     print(main())
